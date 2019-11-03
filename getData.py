e36ca244835c6191c26aac9c6912a5483758c2a5
isa6iah <31462373+isa6iah@users.noreply.github.com>
2019-11-03 07:52:41 -0600

    human detection and image saving added

--- getData.py
+++ getData.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import serial
+import picamera
 ser = serial .Serial('/dev/ttyACM0', 9600)
 ser.write(b'0')
 data= ser.readline()
 
+if (word.find('Human') != -1):
+    camera.capture('person.jpg')
+
 print(data)
-
-
